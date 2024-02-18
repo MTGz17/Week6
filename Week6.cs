5a72a3a0df05f73b59ca151a05b1f41a6a2fba46
MTGz17 <150878018+MTGz17@users.noreply.github.com>
2024-02-18 04:12:28 -0500

    Final Week 6 - Post CreatePrimitive

--- Week6.cs
+++ Week6.cs
@@ -7,58 +7,64 @@ public class Week6 : MonoBehaviour
 
     public int sizeOfForest;
     public int stonesRequired;
-    public GameObject floor;
-    public GameObject [] trees;
-    public GameObject [] stones;
-    int xpos;
-
-
 
     void Start()
     {
-        InitializeVariable();
         CreateGround();
         CreateRandomForest();
         CreatePyramid();
     }
 
-    void InitializeVariable()
-    {
-        Vector3 worldOrigin = new Vector3(0, 0, 0);
-        Vector3 randomTreeSpawnPosition = new Vector3(Random.Range(-10,11), 1, Random.Range(-10,11));
-    }
     void CreateGround()
     {
         Vector3 worldOrigin = new Vector3(0, 0, 0);
-        Instantiate(floor, worldOrigin, Quaternion.identity);
-    }
+        GameObject plane  = GameObject.CreatePrimitive(PrimitiveType.Plane);
 
+        plane.transform.localScale = new Vector3 (5, 1, 5);
+        var planeRenderer = plane.GetComponent<Renderer>();
+        planeRenderer.material.SetColor("_Color", Color.blue);
+
+    }
 
     void CreateRandomForest()
     {
     for (int i = 0; i < (sizeOfForest); i++)
         {
-        Vector3 randomTreeSpawnPosition = new Vector3(Random.Range(-10f,1f), 1, Random.Range(-5f,6f));
-        int randomIndex = Random.Range(0,trees.Length);
-        Instantiate(trees[randomIndex], randomTreeSpawnPosition, Quaternion.identity);
+
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cylinder.transform.position = new Vector3(Random.Range(-10f,1f), 1, Random.Range(-5f,6f));
+
+        var cylinderRenderer = cylinder.GetComponent<Renderer>();
+        cylinderRenderer.material.SetColor("_Color", Color.green);
+        
         }
     }
 
-
     void CreatePyramid()
     {  
-        for(int height = 0; height < stonesRequired; height++)
+        GameObject topcube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        topcube.transform.position = new Vector3(10, stonesRequired-.5f, 0); 
+
+        for(int height = 0; height < (stonesRequired - 1); height++)
         {
-            int length = stonesRequired - height;
-            for (int x = -length; x < length-1; x++)
+            int length = (stonesRequired - 1) - height;
+
+            Color32 randomColor = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 255);
+
+            for (int x = -length; x <= length; x++)
              {
-                for (int z = -length; z < length-1; z++)
+                for (int z = -length; z <= length; z++)
                 {
-                    Vector3 pyramid = new Vector3(x+10, height+.5f, z);
-                Instantiate(stones[0], pyramid, Quaternion.identity);  
+                    if (Mathf.Abs(x) == length || Mathf.Abs(z) == length)
+                    {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(x+10, height+.5f, z); 
+
+                    var cubeRenderer = cube.GetComponent<Renderer>();
+                    cubeRenderer.material.SetColor("_Color", randomColor);
+                    }
                 }
              }
-        }
-        
+        } 
     }
 }
